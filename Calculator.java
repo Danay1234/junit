85dbeff54603f2837a6c7e27823ceac56f4f4380
Mubarek1212 <mubarekmohammed3241@gmail.com>
2024-05-17 14:59:51 +0300

    added the logic for detecting positive non primes.

--- Calculator.java
+++ Calculator.java
@@ -114,6 +114,9 @@ public class Calculator {
 
     public boolean isPrime(int n) {
         if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
         return true;
     }
 
